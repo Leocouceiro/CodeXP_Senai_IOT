81bd34441c21114149e9de58bd80441beb0a6e16
KEVIN SADAO NAKAKITA <45540871820@jpmorgan.sp>
2017-11-29 18:39:02 -0200

    Pequena mod.

--- Projeto3/Projeto3.ino
+++ Projeto3/Projeto3.ino
@@ -3,10 +3,10 @@
 #include <Keypad.h>
 #include <utility/logging.h>
 
-const int pinLedVermelho = 2;
+const int pinLedVermelho = A0;//Modifiquei o pinLedVermelho de 2 para A0
 const int pinLedVerde = A1;
-const int pinoBuzzer = 9;
 const int pinoSensorLuz = A2;
+const int pinoBuzzer = 9;
 
 bool digitandoSenha = false;
 bool alarmeAtivado = false;
@@ -18,9 +18,8 @@ String senhaDigitada = "000";
 const char* senha = "123";
 const byte ROWS = 4; //four rows
 const byte COLS = 3; //three columns
-
-
-byte rowPins[ROWS] = {5, 4, 3, A0}; //connect to the row pinouts of the keypad
+//modifiquei o pino A0 para 2 do "rowPins"
+byte rowPins[ROWS] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {8, 7, 6}; //connect to the column pinouts of the keypad
 char keys[ROWS][COLS] = {
   {'1', '2', '3'},
@@ -31,12 +30,11 @@ char keys[ROWS][COLS] = {
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS ); // possivel problem
 
-// Alterar o último valor para o id do seu kit
 const byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x11 };
 EthernetClient ethclient;
 
 RestClient client = RestClient("192.168.3.186", 3000, ethclient);
-const char* parametros = "sid=AC36135c57a2e51019e7c00f107e9408b0&token=db587f4175c3b6360aebf4a11e8699c3&to=5511981368780&from=17312014022&body=Rickyyy";
+const char* parametros = "sid=AC36135c57a2e51019e7c00f107e9408b0&token=db587f4175c3b6360aebf4a11e8699c3&to=5511981368780&from=17312014022&body=29do11";
 
 String response = "";
 
@@ -46,7 +44,6 @@ String response = "";
   int saida;
   int tentativa;*/
 
-//Setup
 void setup() {
   Serial.begin(9600);
   // Connect via DHCP
@@ -65,7 +62,7 @@ void setup() {
 
   pinMode(pinLedVermelho, OUTPUT);
   pinMode(pinLedVerde, OUTPUT);
-  pinMode(A0, INPUT);
+  pinMode(2, INPUT);
   pinMode(pinoSensorLuz, INPUT);
   pinMode(pinoBuzzer, OUTPUT); // saida do buzzer alertando entrada de intruso
 
@@ -117,7 +114,7 @@ void loop() {
           saida += 1;
           } else {
           entrada += 1;
-          }*/
+         }*/
 
       } else {
         numeroSenhaTentativas = numeroSenhaTentativas + 1;
